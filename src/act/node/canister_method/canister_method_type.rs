f7916855dc5efe60d1985ee12fa1dd50516df21e
Dan Steren <dan@dansteren.com>
2023-02-28 11:33:09 -0700

    Move CanisterMethod and CanisterMethodType

---
+++ src/act/node/canister_method/canister_method_type.rs
@@ -0,0 +1,26 @@
+use std::fmt;
+
+#[derive(Clone, PartialEq)]
+pub enum CanisterMethodType {
+    Heartbeat,
+    Init,
+    InspectMessage,
+    PostUpgrade,
+    PreUpgrade,
+    Query,
+    Update,
+}
+
+impl fmt::Display for CanisterMethodType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            CanisterMethodType::Heartbeat => write!(f, "Heartbeat"),
+            CanisterMethodType::Init => write!(f, "Init"),
+            CanisterMethodType::InspectMessage => write!(f, "InspectMessage"),
+            CanisterMethodType::PostUpgrade => write!(f, "PostUpgrade"),
+            CanisterMethodType::PreUpgrade => write!(f, "PreUpgrade"),
+            CanisterMethodType::Query => write!(f, "Query"),
+            CanisterMethodType::Update => write!(f, "Update"),
+        }
+    }
+}
