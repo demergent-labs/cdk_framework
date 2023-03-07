91daa069323bd6f2e91b4b70bc6d8423bfcb2e43
Benjamin DeMann <benjamin.demann@gmail.com>
2023-03-07 15:10:32 -0700

    organize record, tuples and variants

---
+++ src/act/node/candid/record/record.rs
@@ -0,0 +1,82 @@
+use quote::{quote, ToTokens};
+
+use super::Member;
+use crate::{
+    act::node::{
+        candid::type_annotation::{ToTypeAnnotation, TypeAnnotation},
+        declaration::Declare,
+        traits::has_members::HasMembers,
+        CandidType, Declaration,
+    },
+    traits::ToIdent,
+};
+
+#[derive(Clone, Debug)]
+pub struct Record {
+    pub name: Option<String>,
+    pub members: Vec<Member>,
+}
+
+impl Record {
+    fn get_name(&self, parental_prefix: String) -> String {
+        match &self.name {
+            Some(name) => name.clone(),
+            None => format!("{parental_prefix}Record"),
+        }
+    }
+}
+
+impl ToTypeAnnotation<Vec<String>> for Record {
+    fn to_type_annotation(&self, _: &Vec<String>, parental_prefix: String) -> TypeAnnotation {
+        match &self.name {
+            Some(name) => name.clone(),
+            None => format!("{parental_prefix}Record"),
+        }
+        .to_ident()
+        .to_token_stream()
+    }
+}
+
+impl Declare<Vec<String>> for Record {
+    fn to_declaration(
+        &self,
+        keyword_list: &Vec<String>,
+        parental_prefix: String,
+    ) -> Option<Declaration> {
+        let record_ident = self.get_name(parental_prefix.clone()).to_ident();
+        let member_token_streams: Vec<_> = self
+            .members
+            .iter()
+            .enumerate()
+            .map(|(index, member)| {
+                member.to_token_stream(
+                    keyword_list,
+                    self.create_member_prefix(index, self.get_name(parental_prefix.clone())),
+                )
+            })
+            .collect();
+        Some(quote!(
+            #[derive(serde::Deserialize, Debug, candid::CandidType, Clone, CdkActTryIntoVmValue, CdkActTryFromVmValue)]
+            struct #record_ident {
+                #(#member_token_streams),*
+            }
+        ))
+    }
+
+    fn collect_inline_declarations(
+        &self,
+        keyword_list: &Vec<String>,
+        parental_prefix: String,
+    ) -> Vec<Declaration> {
+        self.collect_member_inline_declarations(keyword_list, self.get_name(parental_prefix))
+    }
+}
+
+impl HasMembers for Record {
+    fn get_members(&self) -> Vec<CandidType> {
+        self.members
+            .iter()
+            .map(|member| member.candid_type.clone())
+            .collect()
+    }
+}
---
+++ src/act/node/candid/tuple/tuple.rs
@@ -0,0 +1,86 @@
+use proc_macro2::TokenStream;
+use quote::{quote, ToTokens};
+
+use super::Member;
+use crate::{
+    act::node::{
+        candid::type_annotation::{ToTypeAnnotation, TypeAnnotation},
+        declaration::Declare,
+        traits::has_members::HasMembers,
+        CandidType, Declaration,
+    },
+    traits::ToIdent,
+};
+
+#[derive(Clone, Debug)]
+pub struct Tuple {
+    pub name: Option<String>,
+    pub members: Vec<Member>,
+}
+
+impl Tuple {
+    fn get_name(&self, parental_prefix: String) -> String {
+        match &self.name {
+            Some(name) => name.clone(),
+            None => format!("{parental_prefix}Tuple"),
+        }
+    }
+}
+
+impl<C> ToTypeAnnotation<C> for Tuple {
+    fn to_type_annotation(&self, _: &C, parental_prefix: String) -> TypeAnnotation {
+        self.get_name(parental_prefix).to_ident().to_token_stream()
+    }
+}
+
+impl Declare<Vec<String>> for Tuple {
+    fn to_declaration(
+        &self,
+        keyword_list: &Vec<String>,
+        parental_prefix: String,
+    ) -> Option<Declaration> {
+        let tuple_ident = self.get_name(parental_prefix.clone()).to_ident();
+        let member_idents: Vec<TokenStream> = self
+            .members
+            .iter()
+            .enumerate()
+            .map(|(index, member)| {
+                member.to_token_stream(
+                    keyword_list,
+                    self.create_member_prefix(index, self.get_name(parental_prefix.clone())),
+                )
+            })
+            .collect();
+
+        let member_idents = if member_idents.len() == 1 {
+            let member_ident = &member_idents[0];
+            quote!((#member_ident,))
+        } else {
+            quote!(#(#member_idents),*)
+        };
+
+        Some(quote!(
+            #[derive(serde::Deserialize, Debug, candid::CandidType, Clone, CdkActTryIntoVmValue, CdkActTryFromVmValue)]
+            struct #tuple_ident (
+                #member_idents
+            );
+        ))
+    }
+
+    fn collect_inline_declarations(
+        &self,
+        keyword_list: &Vec<String>,
+        parental_prefix: String,
+    ) -> Vec<Declaration> {
+        self.collect_member_inline_declarations(keyword_list, self.get_name(parental_prefix))
+    }
+}
+
+impl HasMembers for Tuple {
+    fn get_members(&self) -> Vec<CandidType> {
+        self.members
+            .iter()
+            .map(|elem| elem.candid_type.clone())
+            .collect()
+    }
+}
