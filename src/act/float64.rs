72612a1ecb93f6b78aa3cf7493e170ac55060275
Jordan Last <jordan.michael.last@gmail.com>
2023-04-12 17:00:30 -0500

    implement Ord, PartialOrd, Eq, and PartialEq on all structs and enums, also implement for f64 and f32 with wrapper type

---
+++ src/act/float64.rs
@@ -0,0 +1,29 @@
+use proc_macro2::TokenStream;
+use quote::quote;
+
+pub fn generate() -> TokenStream {
+    quote! {
+        #[derive(serde::Deserialize, Clone, Debug, candid::CandidType)]
+        struct _CdkFloat64(f64);
+
+        impl std::cmp::PartialOrd for _CdkFloat64 {
+            fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+                self.0.partial_cmp(&other.0)
+            }
+        }
+
+        impl std::cmp::Ord for _CdkFloat64 {
+            fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+                self.partial_cmp(other).unwrap_or(std::cmp::Ordering::Less)
+            }
+        }
+
+        impl PartialEq for _CdkFloat64 {
+            fn eq(&self, other: &Self) -> bool {
+                self.0.eq(&other.0)
+            }
+        }
+
+        impl Eq for _CdkFloat64 {}
+    }
+}
