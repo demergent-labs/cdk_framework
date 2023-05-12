d1d8df531b39a6a9ee59f30678c317842c01af12
Jordan Last <jordan.michael.last@gmail.com>
2023-05-12 14:06:16 -0500

    remove getrandom::register_custom_getrandom

--- src/act/random.rs
+++ src/act/random.rs
@@ -3,21 +3,6 @@ use quote::quote;
 
 pub fn generate_randomness_implementation() -> TokenStream {
     quote! {
-        thread_local! {
-            static _CDK_RNG_REF_CELL: std::cell::RefCell<rand::rngs::StdRng> = std::cell::RefCell::new(rand::SeedableRng::from_seed([0u8; 32]));
-        }
-
-        fn custom_getrandom(_buf: &mut [u8]) -> Result<(), getrandom::Error> {
-            _CDK_RNG_REF_CELL.with(|rng_ref_cell| {
-                let mut rng = rng_ref_cell.borrow_mut();
-                rng.fill(_buf);
-            });
-
-            Ok(())
-        }
-
-        getrandom::register_custom_getrandom!(custom_getrandom);
-
         fn rng_seed() {
             ic_cdk::spawn(async move {
                 let result: ic_cdk::api::call::CallResult<(Vec<u8>,)> = ic_cdk::api::call::call(
@@ -26,14 +11,10 @@ pub fn generate_randomness_implementation() -> TokenStream {
                     ()
                 ).await;
 
-                _CDK_RNG_REF_CELL.with(|rng_ref_cell| {
-                    let mut rng = rng_ref_cell.borrow_mut();
-
-                    match result {
-                        Ok(randomness) => *rng = rand::SeedableRng::from_seed(randomness.0[..].try_into().unwrap()),
-                        Err(err) => panic!(err)
-                    };
-                });
+                match result {
+                    Ok(randomness) => ic_wasi_polyfill::init(u64::from_be_bytes(randomness.0[..8].try_into().unwrap())),
+                    Err(err) => panic!(err)
+                };
             });
         }
     }
