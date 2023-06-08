c6531e2b3e6afa739b376572eeb3a1f4f99ab90f
Benjamin DeMann <benjamin.demann@gmail.com>
2023-06-08 14:11:36 -0600

    add CollectIterResults

--- src/traits/collect_results.rs
+++ src/traits/collect_results.rs
@@ -5,11 +5,11 @@ pub trait CollectResults {
     fn collect_results(self) -> Result<Self::OkValue, Vec<Self::ErrValue>>;
 }
 
-impl<Value, Error> CollectResults for Vec<Result<Value, Vec<Error>>>
-where
-    Value: Clone,
-    Error: Clone,
-{
+pub trait CollectIterResults<T, Error> {
+    fn collect_results(self) -> Result<Vec<T>, Vec<Error>>;
+}
+
+impl<Value, Error> CollectResults for Vec<Result<Value, Vec<Error>>> {
     type OkValue = Vec<Value>;
     type ErrValue = Error;
 
@@ -23,6 +23,27 @@ where
                 Err(errs) => errors.extend(errs),
             };
         }
+
+        if errors.is_empty() {
+            Ok(ok_values)
+        } else {
+            Err(errors)
+        }
+    }
+}
+
+impl<I, T, Error> CollectIterResults<T, Error> for I
+where
+    I: Iterator<Item = Result<T, Vec<Error>>>,
+{
+    fn collect_results(self) -> Result<Vec<T>, Vec<Error>> {
+        let mut errors = Vec::new();
+        let mut ok_values = Vec::new();
+
+        self.for_each(|result| match result {
+            Ok(ok_value) => ok_values.push(ok_value),
+            Err(errs) => errors.extend(errs),
+        });
 
         if errors.is_empty() {
             Ok(ok_values)
