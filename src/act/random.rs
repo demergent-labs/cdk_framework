c25483c026984f29df6f89ff4f524a821d9a06cc
Jordan Last <jordan.michael.last@gmail.com>
2023-06-13 15:28:25 -0500

    add conditional compilation

--- src/act/random.rs
+++ src/act/random.rs
@@ -3,6 +3,45 @@ use quote::quote;
 
 pub fn generate_randomness_implementation() -> TokenStream {
     quote! {
+        #[cfg(all(target_arch = "wasm32", target_os = "unknown"))]
+        thread_local! {
+            static _CDK_RNG_REF_CELL: std::cell::RefCell<rand::rngs::StdRng> = std::cell::RefCell::new(rand::SeedableRng::from_seed([0u8; 32]));
+        }
+
+        #[cfg(all(target_arch = "wasm32", target_os = "unknown"))]
+        fn custom_getrandom(_buf: &mut [u8]) -> Result<(), getrandom::Error> {
+            _CDK_RNG_REF_CELL.with(|rng_ref_cell| {
+                let mut rng = rng_ref_cell.borrow_mut();
+                rng.fill(_buf);
+            });
+
+            Ok(())
+        }
+
+        #[cfg(all(target_arch = "wasm32", target_os = "unknown"))]
+        getrandom::register_custom_getrandom!(custom_getrandom);
+
+        #[cfg(all(target_arch = "wasm32", target_os = "unknown"))]
+        fn rng_seed() {
+            ic_cdk::spawn(async move {
+                let result: ic_cdk::api::call::CallResult<(Vec<u8>,)> = ic_cdk::api::call::call(
+                    candid::Principal::from_text("aaaaa-aa").unwrap(),
+                    "raw_rand",
+                    ()
+                ).await;
+
+                _CDK_RNG_REF_CELL.with(|rng_ref_cell| {
+                    let mut rng = rng_ref_cell.borrow_mut();
+
+                    match result {
+                        Ok(randomness) => *rng = rand::SeedableRng::from_seed(randomness.0[..].try_into().unwrap()),
+                        Err(err) => panic!(err)
+                    };
+                });
+            });
+        }
+
+        #[cfg(all(target_arch = "wasm32", target_os = "wasi"))]
         fn rng_seed() {
             ic_cdk::spawn(async move {
                 let result: ic_cdk::api::call::CallResult<(Vec<u8>,)> = ic_cdk::api::call::call(
