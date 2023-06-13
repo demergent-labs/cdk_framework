5a5acf094e781e81658ae3bda47c9c0592ae2ec7
Jordan Last <jordan.michael.last@gmail.com>
2023-06-13 16:09:59 -0500

    try using the management_canister api directly

--- src/act/random.rs
+++ src/act/random.rs
@@ -44,11 +44,7 @@ pub fn generate_randomness_implementation() -> TokenStream {
         #[cfg(all(target_arch = "wasm32", target_os = "wasi"))]
         fn rng_seed() {
             ic_cdk::spawn(async move {
-                let result: ic_cdk::api::call::CallResult<(Vec<u8>,)> = ic_cdk::api::call::call(
-                    candid::Principal::from_text("aaaaa-aa").unwrap(),
-                    "raw_rand",
-                    ()
-                ).await;
+                let result: ic_cdk::api::call::CallResult<(Vec<u8>,)> = ic_cdk::api::management_canister::main::raw_rand().await;
 
                 match result {
                     Ok(randomness) => ic_wasi_polyfill::init(u64::from_be_bytes(randomness.0[..8].try_into().unwrap())),
