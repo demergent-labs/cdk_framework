dee30cb310d571de0388b21e71288810a6a64e9a
Jordan Last <jordan.michael.last@gmail.com>
2024-03-28 14:48:55 -0500

    remove guard functions from init, inspect message, pre upgrade, and post upgrade

--- src/act/node/canister_method/pre_upgrade_method.rs
+++ src/act/node/canister_method/pre_upgrade_method.rs
@@ -1,30 +1,19 @@
 use proc_macro2::TokenStream;
 use quote::quote;
 
-use crate::{
-    act::{node::Context, Declaration, Declare},
-    traits::WithUserDefinedPrefix,
-};
+use crate::act::{node::Context, Declaration, Declare};
 
 #[derive(Clone)]
 pub struct PreUpgradeMethod {
-    pub guard_function_name: Option<String>,
     pub body: TokenStream,
 }
 
 impl Declare<Context> for PreUpgradeMethod {
     fn to_declaration(&self, _: &Context, _: String) -> Option<Declaration> {
         let body = &self.body;
-        let macro_args = match &self.guard_function_name {
-            Some(guard_function_name) => {
-                let prefixed_guard_function_name = guard_function_name.with_user_defined_prefix();
-                quote! {guard = #prefixed_guard_function_name}
-            }
-            None => quote!(),
-        };
 
         Some(quote! {
-            #[ic_cdk_macros::pre_upgrade(#macro_args)]
+            #[ic_cdk_macros::pre_upgrade]
             fn pre_upgrade() {
                 #body
             }
